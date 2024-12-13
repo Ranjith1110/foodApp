eec8fa8d88fa0bf395df8b9a9924c205515129b1
Ranjith <ranjithram878@gmail.com>
2024-12-13 10:38:22 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { assets } from './assets/frontend_assets/assets'
 
 function App() {
   return (
     <>
     <div className='app'>
     <Navbar />
-    <img src={assets.bag_icon} alt="" />
     </div>
     </>
   )
