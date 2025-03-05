fc5bdf45e37ca6ba629caa9b883e406f1000c08f
Ranjith <ranjithram878@gmail.com>
2025-03-05 22:32:05 +0530

    Route setup done for three page

---
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOrder = () => {
+  return (
+    <>
+        <div className="placeorder">
+            
+        </div>
+    </>
+  )
+}
+
+export default PlaceOrder
