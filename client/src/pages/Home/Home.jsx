7f050cef2ac12eed977a186be582926575ea89f8
Ranjith <ranjithram878@gmail.com>
2025-03-12 22:16:57 +0530

    Context file want to create

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css';
+import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 const Home = () => {
+
+    const [category, setCategory] = useState("All");
+
     return (
         <>
             <div className="home">
-                <h1>Home</h1>
+                <Header />
+                <ExploreMenu category={category} setCategory={setCategory} />
             </div>
         </>
     )
