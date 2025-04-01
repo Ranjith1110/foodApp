a78a26651af209917854e47f33d45b35226acc08
Ranjith <ranjithram878@gmail.com>
2025-04-01 18:06:42 +0530

    Items Update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css';
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDispaly from '../../components/FoodDisplay/FoodDispaly';
 
 const Home = () => {
 
@@ -12,6 +13,7 @@ const Home = () => {
             <div className="home">
                 <Header />
                 <ExploreMenu category={category} setCategory={setCategory} />
+                <FoodDispaly category={category} />
             </div>
         </>
     )
